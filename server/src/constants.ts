25ebdb556bc3984ae04696ad2482e4eba8b3a521
Brijesh Shetty <brij.jhu@gmail.com>
2024-07-21 18:25:32 -0700

    Initial server commit

---
+++ server/src/constants.ts
@@ -0,0 +1,7 @@
+class Constants {
+    static PORT: number = 3000;
+    static CASCADE_URL: string = 'https://cascade.com';
+    static ERROR_MESSAGE: string = 'An error has occurred.';
+}
+
+export default Constants;
